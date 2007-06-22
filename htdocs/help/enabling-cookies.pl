0a0ac86cc40e3031fe3ac9f1f41659ef775fdc58
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2007-06-22 11:35:34 +0000

    added title for page into code

--- htdocs/help/enabling-cookies.pl
+++ htdocs/help/enabling-cookies.pl
@@ -15,7 +15,6 @@ use rusty::Profiles;
 our $rusty = rusty::Profiles->new;
 
 $rusty->{ttml} = "help/enabling-cookies.ttml";
-$rusty->{data}->{title} = "Enabling Cookies";
 
 $rusty->process_template;
 $rusty->exit;
