c55905961cc944e4796605f893e215f9749b9d90
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2007-06-22 14:08:41 +0000

    changed various startup stuff for no entropy etc and added My::ProxyRemoteAddr() for getting the address forwarded by reverse buffering proxy, bring in shared copy of code using Modperl::RegistryLoader

--- lib/startup.pl
+++ lib/startup.pl
@@ -2,34 +2,41 @@
 
 use strict;
 
+use warnings;
+
+$ENV{MOD_PERL} or die "not running under mod_perl!";
+
 use CGI (qw(-oldstyle_urls -private_tempfiles -debug));
 CGI->compile(qw(url self_url param Vars cookie header upload));
 
 use Apache::DBI (); # Apache::DBI preloads DBI as well
 DBI->install_driver("mysql");
 
-if (-d 'C:/website/6d/lib') {
-  use lib qw(C:/website/6d/lib); #home
-} elsif (-d 'E:/web/website/6d/lib') {
-  use lib qw(E:/web/website/6d/lib); #work
-}
-# This doesn't seem to make a difference!
-#require conf::db_conf;
-#my $_DSN = "DBI:" . $conf::db_conf::DBDRIVER
-#. ":database=" . $conf::db_conf::DATABASE
-#. ";host=" . $conf::db_conf::DBHOST
-#. ";port=" . $conf::db_conf::DBPORT;
-#Apache::DBI->setPingTimeOut($_DSN, 300);
 
-# Just in dev, to speed up apache time to restart, we're
-# taking these out!  Put them back in production.
-#use CarpeDiem;
-#use Email;
-#use ImagePwd;
-#use conf::db_conf;
-#use conf::mail_conf;
-##use rusty; # This doesn't work! :)
-##use rusty::Profiles; # Just won't work. :(
+use CarpeDiem;
+use Email;
+use ImagePwd;
+use conf::db_conf;
+use conf::mail_conf;
+#use rusty; # This doesn't work! :)
+#use rusty::Profiles; # Just won't work. :(
+#
+## This doesn't seem to make a difference!
+##require conf::db_conf;
+##my $_DSN = "DBI:" . $conf::db_conf::DBDRIVER
+##. ":database=" . $conf::db_conf::DATABASE
+##. ";host=" . $conf::db_conf::DBHOST
+##. ";port=" . $conf::db_conf::DBPORT;
+##Apache::DBI->setPingTimeOut($_DSN, 300);
+#Apache::DBI->connect_on_init
+#  ("DBI:mysql:database=test;host=localhost",
+#   "user", "password", {
+#                        PrintError => 1, # warn( ) on errors
+#                        RaiseError => 0, # don't die on error
+#                        AutoCommit => 1, # commit executes immediately
+#                       }
+#  );
+
 
 use Template ();
 
@@ -50,43 +57,71 @@ use HTML::FromText ();
 use Data::Validate::URI ();
 use Data::Validate::Email ();
 
+use Time::DaysInMonth ();
+
+if ($ENV{NO_ENTROPY}) {
+  eval {
+    require Math::Random::MT::Auto;
+    import Math::Random::MT::Auto qw( rand srand ), '/dev/urandom';
+  }
+}
 
 # This modules allows compilation of scripts, running under packages derived from ModPerl::RegistryCooker, 
 # at server startup. The script's handler routine is compiled by the parent server, 
 # of which children get a copy and thus saves some memory by initially sharing the compiled copy with the parent 
 # and saving the overhead of script's compilation on the first request in every httpd instance.
-#  {
-#      # test the scripts pre-loading by using trans sub
-#      use ModPerl::RegistryLoader ();
-#      use File::Spec ();
-#      use DirHandle ();
-#      use strict;
-#  
-#      my $dir = File::Spec->catdir(Apache2::ServerUtil::server_root,
-#                                  "cgi-bin");
-#  
-#      sub trans {
-#          my $uri = shift; 
-#          $uri =~ s|^/registry/|cgi-bin/|;
-#          return File::Spec->catfile(Apache2::ServerUtil::server_root,
-#                                     $uri);
-#      }
-#  
-#      my $rl = ModPerl::RegistryLoader->new(
-#          package => "ModPerl::Registry",
-#          trans   => \&trans,
-#      );
-#      my $dh = DirHandle->new($dir) or die $!;
-#  
-#      for my $file ($dh->read) {
-#          next unless $file =~ /\.pl$/;
-#          $rl->handler("/registry/$file");
-#      }
-#  }
-#
-# Not bothering with this because all .pl scripts will be reloaded when changed on disk
-# and as such the shared copy will be useless!  Unless we restart the server each time
-# we change scripts..  Oh it's only cheap memory! Leave it until a problem presents itself!
+# However, all .pl scripts will be reloaded when changed on disk and as such the shared copy will be useless!
+{
+    # test the scripts pre-loading by using trans sub
+    use ModPerl::RegistryLoader ();
+    use File::Spec ();
+    use DirHandle ();
+    
+    
+    # DOCUMENT_ROOT must be set in apache config manually using
+    # PerlSetEnv DOCUMENT_ROOT path.. (DocumentRoot isn't set yet)..
+    my $dir = $1 if $ENV{DOCUMENT_ROOT} =~ /^(.+)$/o;
+    
+    
+    sub trans {
+      my $uri = shift;
+      #$uri =~ s|^/|/htdocs/|;
+      return File::Spec->catfile($dir, $uri);
+    }
+    
+    my $rl = ModPerl::RegistryLoader->new(
+            package => "ModPerl::Registry",
+            trans   => \&trans,
+            #debug   => 1,
+            );
+    my $dh = DirHandle->new($dir) or die $!;
+    
+    for my $file ($dh->read) {
+      next unless $file =~ /^(.+\.pl)$/;
+      $rl->handler("$1");
+    }
+}
+
+
+
+use Apache2::RequestRec ();
+use Apache2::Connection ();
+use Apache2::Const qw(OK);
+
+sub My::ProxyRemoteAddr ($) {
+  my $r = shift;
+  my $c = $r->connection;
+  
+  # we'll only look at the X-Forwarded-For header if the requests
+  # comes from our proxy at localhost
+  return OK unless ($c->remote_ip eq "127.0.0.1");
+  
+  if (my ($ip) = $r->headers_in->{'X-Forwarded-For'} =~ /([^,\s]+)$/o) {
+    $c->remote_ip($ip);
+  }
+  
+  return OK;
+}
 
 
 1;
