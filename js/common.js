18240881508196423f38c39c659b09fc605ededc
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2007-07-20 20:59:46 +0000

    removed addEvent and remomveEvent calls since they are duplicated in another include - also moved the call to AddEvent into the head of the page

--- js/common.js
+++ js/common.js
@@ -8,35 +8,6 @@ function openWindow(url, name, w, h, scrollbars) {
   newWin.focus();
 }
 
-
-
-/* ###########################################
-addEvent function from http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
-*/
-function addEvent( obj, type, fn )
-{
-  if (obj.addEventListener)
-    obj.addEventListener( type, fn, false );
-  else if (obj.attachEvent)
-  {
-    obj["e"+type+fn] = fn;
-    obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
-    obj.attachEvent( "on"+type, obj[type+fn] );
-  }
-}
-
-function removeEvent( obj, type, fn )
-{
-  if (obj.removeEventListener)
-    obj.removeEventListener( type, fn, false );
-  else if (obj.detachEvent)
-  {
-    obj.detachEvent( "on"+type, obj[type+fn] );
-    obj[type+fn] = null;
-    obj["e"+type+fn] = null;
-  }
-}
-
 /*
 Create the new window
 */
@@ -76,7 +47,5 @@ function getNewWindowLinks() {
     objWarningText = null;
   }
 }
-
-addEvent(window, 'load', getNewWindowLinks);
 
 /* ########################################### */
