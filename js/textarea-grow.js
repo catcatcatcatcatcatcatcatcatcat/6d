52e9249b8b0f9e47e68f60a6f310b05e695cc02e
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2007-09-09 18:46:12 +0000

    Changed this whole thing so that min cols are set on the first call and never set again (so taken from the initial DOM) and max rows/max cols are passed in to the function

--- js/textarea-grow.js
+++ js/textarea-grow.js
@@ -1,35 +1,27 @@
 
-var MIN_COLS;
-var MIN_ROWS;
-var MAX_COLS = 80; /* DEFAULT */
-var MAX_ROWS = 45; /* DEFAULT */
+var max_cols = 80; /* DEFAULT */
+var max_rows = 45; /* DEFAULT */
 
 /* Make textareas grow automagically as the user types */
 fixup_textarea_size = function(ta, max_cols, max_rows) {
   
   /* On first call to this fcn, setup the starting values */
+  /* for the min rows and cols as the initial values */
   
-  /* TODO: We should restrict each of these to every element.. not
-   * to global vars for min which will be shared by all textareas */
-  
-  if (MIN_COLS == undefined) {
-    if (ta.cols != undefined)
-      MIN_COLS = ta.cols;
-    else
-      MIN_COLS = 15; /* DEFAULT */
+  if (ta.min_cols == undefined) {
+    ta.min_cols = ta.cols;
   }
-  if (MIN_ROWS == undefined) {
-    if (ta.rows != undefined)
-      MIN_ROWS = ta.rows;
-    else
-      MIN_ROWS = 3; /* DEFAULT */
+  min_cols = ta.min_cols;
+  if (ta.min_rows == undefined) {
+    ta.min_rows = ta.rows;
   }
+  min_rows = ta.min_rows;
   
-  /* Override the max values if they're defined in this call.. */
-  if (max_cols != undefined)
-    MAX_COLS = max_cols;
-  if (max_rows != undefined)
-    MAX_ROWS = max_rows;
+  /* Use the default max values if they're not defined in this call.. */
+  if (max_cols == undefined)
+    max_cols = MAX_COLS;
+  if (max_rows == undefined)
+    max_rows = MAX_ROWS;
   
   
   var text_length = ta.value.length;
@@ -42,13 +34,13 @@ fixup_textarea_size = function(ta, max_cols, max_rows) {
      * array we add 1 row to the TEXTAREA..
      */
     num_rows++;
-    if (lines[ii].length > MAX_COLS-5) {
+    if (lines[ii].length > max_cols-5) {
       /* Within each element in our array, determine whether
-       * the length of text is greater that our MAX_COLS value.
+       * the length of text is greater that our max_cols value.
        * If so, then we need another row for each time that the
-       *  length is greater than the MAX_COLS value.
+       *  length is greater than the max_cols value.
        */
-      num_rows += Math.floor(lines[ii].length/MAX_COLS)
+      num_rows += Math.floor(lines[ii].length/max_cols)
     }
   }
   
@@ -56,27 +48,27 @@ fixup_textarea_size = function(ta, max_cols, max_rows) {
     /* If there is no text in the TEXTAREA we default to our
      * minimum values.
      */
-    ta.cols = MIN_COLS;
-    ta.rows = MIN_ROWS;
+    ta.cols = min_cols;
+    ta.rows = min_rows;
   } else {
     /* If there is only 1 row, then all we need to determine is
      * how many COLS we need.  It will be somewhere between our
-     * MIN_COLS & MAX_COLS values.
+     * min_cols & max_cols values.
      */
     if (num_rows <= 1) {
-      ta.rows = MIN_ROWS;
-      ta.cols = (text_length % MAX_COLS) + 1 >= MIN_COLS 
-             ? ((text_length % MAX_COLS) + 1) 
-             : MIN_COLS ;
+      ta.rows = min_rows;
+      ta.cols = (text_length % max_cols) + 1 >= min_cols 
+             ? ((text_length % max_cols) + 1) 
+             : min_cols ;
     } else {
       /* If there is more than 1 row then we immediately
-       * default to our MAX_COLS value, and then determine
+       * default to our max_cols value, and then determine
        * how many ROWS we need.
        */
       num_rows++; /* Up the number of rows so we have one spare always */
-      ta.cols = MAX_COLS;
-      ta.rows = (num_rows > MAX_ROWS ? MAX_ROWS :
-                  (num_rows < MIN_ROWS-1 ? MIN_ROWS :
+      ta.cols = max_cols;
+      ta.rows = (num_rows > max_rows ? max_rows :
+                  (num_rows < min_rows-1 ? min_rows :
                     num_rows));
     }
   }
