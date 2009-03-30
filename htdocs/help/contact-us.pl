87ffa0cbdb196d5d5cd0858b7d90e95f01456506
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2009-03-30 11:10:19 +0000

    Adding logic to add passphrase-image captcha thingemy to all support emails.  I hate spambots

--- htdocs/help/contact-us.pl
+++ htdocs/help/contact-us.pl
@@ -16,10 +16,12 @@ use rusty::Profiles;
 
 our $rusty = rusty::Profiles->new;
 
-use vars qw ( $email $confirmemail $name $phone $dept $problem $subject $description $ref $dept_options );
+use vars qw ( $email $confirmemail $name $phone $dept $problem $subject $description $ref $dept_options $passphrase );
 
 
 # Subroutine prototypes
+
+sub generate_passphrase(@);
 
 sub email_support();
 
@@ -37,8 +39,11 @@ $dept = $rusty->{data}->{dept} = $rusty->{params}->{dept};
 $problem = $rusty->{data}->{problem} = $rusty->{params}->{problem};
 $subject = $rusty->{data}->{subject} = $rusty->{params}->{subject};
 $description = $rusty->{data}->{description} = $rusty->{params}->{description};
+$passphrase = $rusty->{data}->{passphrase} = lc($rusty->{params}->{passphrase});
 
 $ref = $rusty->{core}->{'ref'} = $rusty->{params}->{'ref'};
+
+my $num_param_errors = 0;
 
 if ($rusty->{params}->{'error'}) {
   
@@ -55,8 +60,6 @@ if ($rusty->{params}->{'error'}) {
     $rusty->process_template();
     $rusty->exit;
   }
-  
-  # If the user has entered a profile name
   
   if (!$email) {
     $rusty->{data}->{'error'} = "noemail";
@@ -94,7 +97,105 @@ if ($rusty->{params}->{'error'}) {
     #print $rusty->redirect( -url => $rusty->CGI->url( -relative => 1 )
     #                                   . "?error=hackedform" );
     #$rusty->exit;
+  } elsif (!$passphrase) {
+    $rusty->{data}->{'error'} = "nopassphrase";
+    #print $rusty->redirect( -url => $rusty->CGI->url( -relative => 1 )
+    #                                   . "?error=nopassphrase" );
+    #$rusty->exit;
   } else {
+    
+    # Verify that the passphrase matches
+    $query = <<ENDSQL
+SELECT passphrase
+FROM `signup~passphrase`
+WHERE passphrase_id = ?
+ENDSQL
+;
+    $sth = $rusty->DBH->prepare_cached($query);
+    $sth->execute($rusty->{params}->{passphrase_id});
+    my $passphrase = $sth->fetchrow_array();
+    $sth->finish;
+    
+    if (!$passphrase) {
+      
+      warn "Passphrase id '$rusty->{params}->{passphrase_id}' expired.";
+      
+      $rusty->{param_errors}->{passphrase}->{error} =
+	'previous passphrase has expired - please enter the new passphrase';
+      $rusty->{param_errors}->{passphrase}->{title} =
+        $rusty->{param_info}->{passphrase}->{title};
+      $num_param_errors++;
+      
+      $rusty->{params}->{passphrase_id} = generate_passphrase();
+      
+      $rusty->{params}->{passphrase} = '';
+      
+    } elsif ($num_param_errors == 0) {
+      
+      my $passphrase_success_field = 'passphrase_hit';
+      
+      if ($rusty->{params}->{passphrase} ne $passphrase) {
+        
+        # Only if the form has no other errors, then check the passphrase.
+	# If the passphrase does not match then generate a new passphrase.
+	$passphrase_success_field = 'passphrase_miss';
+	
+	# Was this passphrase attempt a near miss?  Or just plain rubbish?
+        for (my $i=0; $i<=length($passphrase)-3; $i++) {
+          my $chunk = substr($passphrase, $i, 3);
+          if ($rusty->{params}->{passphrase} =~ /\Q$chunk\E/) {
+            $passphrase_success_field = 'passphrase_near_miss';
+            last;
+          }
+        }
+        
+        warn "Passphrase '$passphrase' did not match user's attempt '".
+             $rusty->{params}->{passphrase}."'." . ($passphrase_success_field eq 'passphrase_near_miss' ? ' But it was very close!' : '');
+        
+        $rusty->{param_errors}->{passphrase}->{error} =
+          'passphrase was not correct - please enter the new passphrase';
+        $rusty->{param_errors}->{passphrase}->{title} =
+          $rusty->{param_info}->{passphrase}->{title};
+        $num_param_errors++;
+        
+        $rusty->{params}->{passphrase} = '';
+        
+        # Generate new password for this session
+        generate_passphrase($rusty->{params}->{passphrase_id});
+      }
+      
+      # Build query to log successful passphrase hits as well as misses in stats db.
+      $query = <<ENDSQL
+INSERT DELAYED INTO `site~stats`
+SET $passphrase_success_field = 1,
+    date = CURRENT_DATE()
+ON DUPLICATE KEY UPDATE $passphrase_success_field = $passphrase_success_field + 1
+ENDSQL
+;
+      $sth = $rusty->DBH->prepare_cached($query);
+      $sth->execute();
+      $sth->finish;
+    }
+    
+  if ($num_param_errors > 0) {
+    
+    # If errors in form, print signup form with errors flagged.
+    $rusty->{data}->{errors} = $rusty->{param_errors};
+    $rusty->process_template;
+    $rusty->exit;
+    
+  } else {
+    
+    # If the form was filled out correctly;
+    # First, remove old passphrase session so it cannot be re-used.
+    $query = <<ENDSQL
+DELETE FROM `signup~passphrase`
+WHERE passphrase_id = ?
+ENDSQL
+;
+    $sth = $rusty->DBH->prepare_cached($query);
+    $sth->execute($rusty->{params}->{passphrase_id});
+    $sth->finish;
     
     email_support();
     print $rusty->redirect( -url => $rusty->CGI->url( -relative => 1 )
@@ -117,12 +218,62 @@ ENDSQL
     $sth->execute($rusty->{core}->{user_id});
     ($rusty->{core}->{user_info}->{email}, $rusty->{core}->{user_info}->{real_name}) = $sth->fetchrow_array;
     $sth->finish;
+    
+    # If this is the 1st call to signup, generate
+    # a new passphrase and print blank signup form
+    $rusty->{data}->{passphrase_id} = generate_passphrase();
   }
   
 }
 
 $rusty->process_template;
 $rusty->exit;
+
+
+
+
+sub generate_passphrase(@) {
+  
+  my $phrase_id = shift;
+  
+  my $phrase = $rusty->random_word();
+  
+  if ($phrase_id) {
+    
+    # If we were handed a passphrase id, we are
+    # generating a new passphrase for this session
+    $query = <<ENDSQL
+UPDATE `signup~passphrase`
+SET passphrase = ?
+WHERE passphrase_id = ?
+ENDSQL
+;
+    $sth = $rusty->DBH->prepare_cached($query);
+    $sth->execute($phrase, $phrase_id);
+    $sth->finish;
+    
+  } else {
+    
+    # Otherwise, we are creating a brand new passphrase
+    # (in event of new signup or passphrase expiry).
+    # So put generated passphrase with id into db
+    # and, if successful, return the associated id.
+    $phrase_id = $ENV{'UNIQUE_ID'};
+    
+    $query = <<ENDSQL
+INSERT INTO `signup~passphrase`
+( passphrase_id, passphrase )
+VALUES
+( ?, ? )
+ENDSQL
+;
+    $sth = $rusty->DBH->prepare_cached($query);
+    $sth->execute($phrase_id, $phrase);
+    $sth->finish;
+  }
+  
+  return $phrase_id;
+}
 
 
 
