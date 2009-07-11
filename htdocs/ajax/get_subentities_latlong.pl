4254cf79ada23069d91dc638374b5316519fa392
rusty <rusty@c467baf2-8e1a-0410-9ce2-cb22004efe28>
2009-07-11 17:44:25 +0000

    Removing references to DST offset

--- htdocs/ajax/get_subentities_latlong.pl
+++ htdocs/ajax/get_subentities_latlong.pl
@@ -16,23 +16,12 @@ my $DBH = rusty::db_connect();
 
 my $params = rusty::get_utf8_params();
 
-
-
-#my $query = <<ENDSQL
-#SELECT SQL_CACHE subentity_code, subentity_name, latitude, longitude
-#FROM `lookup~continent~country~city1000`
-#WHERE country_code = ?
-#  AND latitude IS NOT NULL
-#  AND longitude IS NOT NULL
-#ORDER BY subentity_name
-#ENDSQL
-#;
 my $query = <<ENDSQL
 SELECT SQL_CACHE c.subentity_code, c.name AS subentity_name,
        c.latitude, c.longitude,
        c.population, c.elevation,
        c.TimeZoneId AS timezone,
-       t.GMT_offset_2007_01_01, t.DST_offset_2007_07_01,
+       t.GMT_offset_2007_01_01 AS gmt_offset,
        IF(c.latitude>=0,
           CONCAT(FORMAT(c.latitude,3),'&deg;N'),
           CONCAT(FORMAT(c.latitude*-1,3),'&deg;S')) AS latitude_formatted,
@@ -52,14 +41,13 @@ $sth->execute($params->{country_code});
 my $output_text = "";
 use HTML::Entities 'encode_entities';
 while (my ($subentity_code, $subentity_name, $latitude, $longitude,
-           $population, $elevation, $timezone, $gmt_offset, $dst_offset,
+           $population, $elevation, $timezone, $gmt_offset,
            $latitude_formatted, $longitude_formatted) = $sth->fetchrow_array) {
-  $gmt_offset =~ s/\.0$//o; $gmt_offset =~ s/^(?!-)/\+/;
-  $dst_offset =~ s/\.0$//o; $dst_offset =~ s/^(?!-)/\+/;
+  $gmt_offset =~ s/\.0$//o; $gmt_offset =~ s/^(?!-)/\+/; $gmt_offset = 'GMT' . $gmt_offset;
   $elevation ||= 'unknown';
   utf8::decode($subentity_name);
   $output_text .= $subentity_code.'|'.HTML::Entities::encode_entities($subentity_name).'|'.$latitude.'|'.$longitude.'|'
-                 .$population.'|'.$elevation.'|'.$timezone.'|'."GMT${gmt_offset}/DST${dst_offset}".'|'
+                 .$population.'|'.$elevation.'|'.$timezone.'|'.$gmt_offset.'|'
                  .$latitude_formatted.'|'.$longitude_formatted.'||';
 }
 
